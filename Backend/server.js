77bc0cb75695b5edf7d485e1004935c885c5a6e4
chidiebereojingwa <ojingwaoji@gmail.com>
2023-12-27 22:03:12 +0000

    Add routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,10 @@
 const express = require('express');
-const dontenv = require('dotenv').config()
+const dontenv = require('dotenv').config();
 const port = process.env.PORT || 5000
 
 const app = express()
 
-
-app.get('/api/goals', (req, res)=>{
-    res.send('Hi Governor')
-})
+app.use('/api/goals', require('./routes/goalRoutes'));
 
 
 app.listen(port, () => console.log(`server started on port ${port}`))
